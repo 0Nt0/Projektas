994509c24ab1f3ed03335eeeec82ea1de268e517
ToriNtHr <toma.ziliute@stud.viko.lt>
2022-12-29 15:48:45 +0200

    User

---
+++ New folder/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getDatabase} from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBDR7zdSeP_d7-Yvr4LmPUfoKjuWVgWzu8",
+  authDomain: "hibridiniu-projektas.firebaseapp.com",
+  databaseURL: "https://hibridiniu-projektas-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hibridiniu-projektas",
+  storageBucket: "hibridiniu-projektas.appspot.com",
+  messagingSenderId: "808104601288",
+  appId: "1:808104601288:web:a7226ed9072589971467fd",
+  measurementId: "G-X34Z68NCDE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db=getDatabase(app);
